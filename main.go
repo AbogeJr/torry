d99b46ad84617ce0c1021bdd8460bc7f4a168e4b
AbogeJr <junioraboge@gmail.com>
2025-05-26 00:35:53 +0300

    feat: added structs to parse torrent file

--- main.go
+++ main.go
@@ -3,37 +3,76 @@ package main
 import (
 	"fmt"
 	"os"
+
 	bencode "github.com/jackpal/bencode-go"
 )
 
-type Email struct {
-	Where string
-	Addr string
+type TorrentFile struct {
+	Announce    string
+	InfoHash    [20]byte
+	PieceHashes [][20]byte
+	PieceLength int
+	Length      int
+	Name        string
 }
 
-type Result struct {
-	Name string
-	Phone string
-	Email []Email
+type bencodeTorrentInfo struct {
+	Pieces      string `bencode:"pieces"`
+	PieceLength int    `bencode:"piece length"`
+	Length      int    `bencode:"length"`
+	Name        string `bencode:"name"`
 }
 
-func main(){
-	inputPath := os.Args[1]
-	outputPath := os.Args[2]
-	var r = Result{}
-	fmt.Println(inputPath, outputPath)
+type bencodeTorrent struct {
+	Announce string             `bencode:"announce"`
+	Info     bencodeTorrentInfo `bencode:"info"`
+}
 
-	file, err := os.Open(inputPath)
+func (btfo *bencodeTorrent) toProcessedTorrentFile() (TorrentFile, error) {
+	t := TorrentFile{
+		Announce: btfo.Announce,
+		// InfoHash: [],
+		// PieceHashes: [],
+		PieceLength: btfo.Info.PieceLength,
+		Length:      btfo.Info.Length,
+		Name:        btfo.Info.Name,
+	}
+
+	return t, nil
+}
+
+func openTorrentFile(filePath string) (TorrentFile, error) {
+
+	file, err := os.Open(filePath)
 
 	if err != nil {
 		fmt.Println("Error opening torrent file", err)
 	}
+
 	defer file.Close()
-	
-	err =  bencode.Unmarshal(file, &r)
+
+	bt := bencodeTorrent{}
+
+	err = bencode.Unmarshal(file, &bt)
+
 	if err != nil {
 		fmt.Println("Error Unmarshalling:", err)
 	}
-	
-	fmt.Printf("%+v\n", r)
+
+	return bt.toProcessedTorrentFile()
+}
+
+func main() {
+	inputPath := os.Args[1]
+	outputPath := os.Args[2]
+
+	torrentFile, err := openTorrentFile(inputPath)
+
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(outputPath, torrentFile)
+
+	fmt.Printf("%+v\n", torrentFile)
 }
