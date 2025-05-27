f24eb05df7c79312934c0cdd4dbdfe30ff0d4cbd
AbogeJr <junioraboge@gmail.com>
2025-05-27 22:21:03 +0300

    feat: added handshake struct and funcs (read and serialize)

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ import (
 	"crypto/sha1"
 	"encoding/binary"
 	"fmt"
+	"io"
 	"net"
 	"net/http"
 	"net/url"
@@ -44,6 +45,61 @@ type trackerURLResponse struct {
 type Peer struct {
 	IP   net.IP
 	Port uint16
+}
+
+type Handshake struct {
+	Pstr     string
+	Infohash [20]byte
+	PeerId   [20]byte
+}
+
+func (h *Handshake) Serialize() []byte {
+	// note: the bittorrent handshake will be 68 bytes long
+	buf := make([]byte, len(h.Pstr)+49)
+
+	buf[0] = byte(len(h.Pstr))
+	currentIndex := 1 //note: buff[0] is currently holding our pstr length
+	currentIndex += copy(buf[currentIndex:], []byte(h.Pstr))
+	currentIndex += copy(buf[currentIndex:], make([]byte, 8))
+	currentIndex += copy(buf[currentIndex:], h.Infohash[:])
+	currentIndex += copy(buf[currentIndex:], h.PeerId[:])
+
+	return buf
+}
+
+func ReadHandshake(r io.Reader) (*Handshake, error) {
+	pstrLengthBuffer := make([]byte, 1)
+
+	_, err := io.ReadFull(r, pstrLengthBuffer)
+
+	if err != nil {
+		return &Handshake{}, err
+	}
+
+	pstrlen := int(pstrLengthBuffer[0])
+
+	if pstrlen == 0 {
+		fmt.Println("pstr length cannot be 0")
+	}
+
+	handshakeBuff := make([]byte, pstrlen+48)
+	_, err = io.ReadFull(r, handshakeBuff)
+	if err != nil {
+		return &Handshake{}, err
+	}
+
+	var peerId, infoHash [20]byte
+
+	copy(infoHash[:], handshakeBuff[pstrlen+8:pstrlen+8+20])
+	copy(peerId[:], handshakeBuff[pstrlen+8:pstrlen+8+20+20])
+
+	h := Handshake{
+		Pstr:     string(handshakeBuff[:pstrlen]),
+		Infohash: infoHash,
+		PeerId:   peerId,
+	}
+
+	return &h, nil
 }
 
 func UnmarshallPeers(binPeerList []byte) ([]Peer, error) {
@@ -224,13 +280,26 @@ func main() {
 		note: casting the bencoded peers binary string to a slice of bytes for
 		processing/unmarshalling
 	*/
-	peers, err := UnmarshallPeers([]byte(trackerResponse.Peers))
+	// peers, err := UnmarshallPeers([]byte(trackerResponse.Peers))
 
+	// if err != nil {
+	// 	fmt.Println("Error unmarshalling peers", err)
+	// }
+
+	peerHandshake := Handshake{
+		Pstr:     "Bittorrent Protocol",
+		Infohash: torrentFile.InfoHash,
+		PeerId:   peerID,
+	}
+	serializedHandshake := peerHandshake.Serialize()
+
+	// fmt.Println(peers)
+	// fmt.Println(serializedHandshake)
+	reader := bytes.NewReader(serializedHandshake)
+	hs, err := ReadHandshake(reader)
 	if err != nil {
-		fmt.Println("Error unmarshalling peers", err)
+		fmt.Println("Error reading handshake")
 	}
+	fmt.Printf("%+v\n", *hs)
 
-	for i := range len(peers) {
-		fmt.Printf("%+v\n", peers[i].IP)
-	}
 }
